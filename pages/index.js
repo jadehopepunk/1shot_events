7a350d07bea464ba3d06ae5f9bfb23b136d05e41
Craig Ambrose <craig@craigambrose.com>
2020-10-05 20:47:07 +1100

    Added hero section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+const EmojiWord = ({ children }) => (
+  <strong className="font-semibold" style={{ whiteSpace: "nowrap" }}>
+    {children}
+  </strong>
+);
+
+const Hero = () => (
+  <div className="bg-blue-400 px-4 py-10">
+    <div className="flex flex-col align-middle">
+      <h1 className="text-5xl text-center font-medium mb-2">1Shot</h1>
+      <p className="text-3xl text-center mb-4">
+        Create a free, untracked <EmojiWord>event 🗓️</EmojiWord> for your <EmojiWord>friends 😎</EmojiWord> that{" "}
+        <EmojiWord>deletes itself 👻</EmojiWord> when you're done
+      </p>
+      <button class="bg-transparent text-blue-800 font-semibold py-2 px-4 flex flex-row justify-center">
+        more details
+        <svg className="h-6 w-6 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+        </svg>
+      </button>
+    </div>
+  </div>
+);
 
 export default function Home() {
   return (
-    <div className="p-4 shadow rounded bg-white">
-    <h1 className="text-purple-500 leading-normal">Next.js is ok</h1>
-    <p className="text-gray-500">with Tailwind CSS</p>
-  </div>
+    <div>
+      <Hero />
+    </div>
   );
 }
